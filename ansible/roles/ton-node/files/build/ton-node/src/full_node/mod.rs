e26ef678303adf3a2bb21dfba94c3ac361560dca
root <root@baremetal01.fra02.ibm.cloud>
2020-08-30 17:36:13 -0500

    Add src clone

--- ansible/roles/ton-node/files/build/ton-node/src/full_node/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod state_helper;
-pub mod apply_block;
-pub mod shard_client;
--- ansible/roles/ton-node/files/build/ton-node/src/network/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod node_network;
-pub mod neighbours;
-pub mod full_node_client;
-pub mod full_node_service;
-#[cfg(features = "local_test")]
-pub mod node_network_stub;
